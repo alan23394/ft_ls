798908b216408a7f1cc342a0f04aece78bddd0e9
Alan Barnett <abarnett@e1z2r5p20.42.us.org>
2019-01-14 23:57:20 -0800

    Add binary tree struct to header file

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,13 +6,97 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 18:23:26 by abarnett          #+#    #+#             */
-/*   Updated: 2019/01/07 18:50:32 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/01/14 23:36:47 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
+typedef struct			s_binarytree
+{
+	char				*string;
+	struct s_binarytree	*left;
+	struct s_binarytree	*right;
+
+}						t_binarytree;
+
 #include "libftprintf.h"
+
+/*
+** libftprintf.h includes libft.h
+** libft.h includes unistd.h and stdlib.h
+**
+** Allowed functions from unistd.h:
+** readlink
+** write
+**
+** Allowed functions from stdlib.h:
+** malloc
+** free
+*/
+
+#include <dirent.h>
+
+/*
+** Allowed functions from dirent.h:
+** opendir
+** readdir
+** closdir
+*/
+
+#include <sys/stat.h>
+
+/*
+** Allowed functions from sys/stat.h:
+** stat
+**	int stat(const char *restrict path, struct stat *restrict buf);
+**	stat obtains information about the file pointed to by path
+**
+** lstat
+**	int lstat(const char *restrict path, struct stat *restrict buf);
+**	lstat does the same as stat, but for a symbolic link
+*/
+
+#include <sys/types.h>
+#include <pwd.h>
+#include <uuid/uuid.h>
+#include <grp.h>
+
+/*
+** Allowed functions from those:
+** getpwuid
+** getgrgid
+*/
+
+#include <sys/xattr.h>
+
+/*
+** Allowed functions from sys/xattr.h:
+** listxattr
+** getxattr
+*/
+
+#include <time.h>
+
+/*
+** Allowed functions from time.h:
+** time
+** ctime
+*/
+
+#include <stdio.h>
+
+/*
+** Allowed functions from stdio.h:
+** perror
+*/
+
+#include <string.h>
+
+/*
+** Allowed functions from string.h:
+** strerror
+*/
 
 #endif
