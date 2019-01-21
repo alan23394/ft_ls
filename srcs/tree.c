4af5e1107ce7be8fbf5cb327016fa4788ae43fe6
Alan Barnett <alanbarnett328@gmail.com>
2019-01-21 06:08:42 -0800

    Move tree loader into tree file

--- srcs/tree.c
+++ srcs/tree.c
@@ -6,7 +6,7 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 00:05:47 by abarnett          #+#    #+#             */
-/*   Updated: 2019/01/17 00:48:06 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/01/21 06:05:30 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,49 @@ void			insert_tree(t_binarytree **files, const char *insert,
 	}
 }
 
+/*
+** Things to preserve in directory struct
+** total blocks
+** width of longest name
+**
+** Things to preserve in filename struct
+** name
+** rights stuff?
+** user name
+** group name
+** links
+** # of bytes
+** date modified
+*/
+
+t_binarytree	*load_tree(t_dirtree *dirtree, char *dir_str, int flags,
+					int (*compare)())
+{
+	DIR				*dir_p;
+	struct dirent	*dir_ent;
+	t_binarytree	*files;
+	struct stat		stats;
+	char			*new_dname;
+
+	files = 0;
+	dir_p = opendir(dir_str);
+	if (!dir_p)
+		return (0);
+	while ((dir_ent = readdir(dir_p)) != 0)
+	{
+		if (!F_ALL(flags) && dir_ent->d_name[0] == '.')
+			continue;
+		insert_tree(&files, ft_strdup(dir_ent->d_name), compare);
+		new_dname = get_dirname(dirtree->dirname, dir_ent->d_name);
+		if (F_RECUR(flags) && stat(new_dname, &stats) == 0 &&
+				S_ISDIR(stats.st_mode))
+			insert_dir(&(dirtree->right), new_dname, compare);
+		else
+			ft_strdel(&new_dname);
+	}
+	closedir(dir_p);
+	return (files);
+}
 
 void			delete_tree(t_binarytree **tree)
 {
