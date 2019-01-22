9216a754b05eeb15b895742ab5af59c151add240
Alan Barnett <alanbarnett328@gmail.com>
2019-01-22 00:37:34 -0800

    Remove functions
    Removing/changing unneeded things, and moving binary tree things to a
    separate, generic binary tree implementation for convenience.

--- srcs/tree.c
+++ srcs/tree.c
@@ -6,22 +6,11 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 00:05:47 by abarnett          #+#    #+#             */
-/*   Updated: 2019/01/21 06:05:30 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/01/22 00:34:17 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
-
-t_binarytree	*new_item(const char *item)
-{
-	t_binarytree	*leaf;
-
-	leaf = (t_binarytree *)malloc(sizeof(t_binarytree));
-	leaf->string = (char *)item;
-	leaf->left = 0;
-	leaf->right = 0;
-	return (leaf);
-}
 
 void			print_tree(t_binarytree *files)
 {
@@ -35,26 +24,6 @@ void			print_tree(t_binarytree *files)
 		if (files->right)
 		{
 			print_tree(files->right);
-		}
-	}
-}
-
-void			insert_tree(t_binarytree **files, const char *insert,
-					int (*compare)())
-{
-	if (!*files)
-	{
-		*files = new_item(insert);
-	}
-	else
-	{
-		if (compare(insert, (*files)->string) >= 0)
-		{
-			insert_tree(&(*files)->right, insert, compare);
-		}
-		else
-		{
-			insert_tree(&(*files)->left, insert, compare);
 		}
 	}
 }
@@ -101,21 +70,4 @@ t_binarytree	*load_tree(t_dirtree *dirtree, char *dir_str, int flags,
 	}
 	closedir(dir_p);
 	return (files);
-}
-
-void			delete_tree(t_binarytree **tree)
-{
-	if (*tree)
-	{
-		if ((*tree)->left)
-		{
-			delete_tree(&(*tree)->left);
-		}
-		if ((*tree)->right)
-		{
-			delete_tree(&(*tree)->left);
-		}
-		ft_strdel(&(*tree)->string);
-		ft_memdel((void **)tree);
-	}
 }
