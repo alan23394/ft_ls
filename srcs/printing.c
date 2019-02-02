2572181b941142beeef55c2fc0aff74e57dc8cf4
Alan Barnett <alanbarnett328@gmail.com>
2019-02-01 19:28:11 -0800

    Simple long output

--- srcs/printing.c
+++ srcs/printing.c
@@ -6,7 +6,7 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/01 14:16:52 by abarnett          #+#    #+#             */
-/*   Updated: 2019/02/01 14:21:28 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/02/01 18:41:18 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,6 @@ void	print_file_long(t_file *file)
 {
 	if (file)
 	{
-		ft_printf("%s %s\n", file->rights, file->name);
+		ft_printf("%s %s %s\n", file->rights, file->date, file->name);
 	}
 }
