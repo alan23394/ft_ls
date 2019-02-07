c6bd14525fcde61819ec535c27e6e1bf387b3440
Alan Barnett <alanbarnett328@gmail.com>
2019-02-06 18:03:47 -0800

    Separate ft_strcmp_rev into a different file

--- libft/srcs/ft_strcmp_rev.c
+++ libft/srcs/ft_strcmp_rev.c
@@ -1,18 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strcmp_rev.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/22 11:03:49 by abarnett          #+#    #+#             */
-/*   Updated: 2018/06/05 10:45:59 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/01/17 00:55:15 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strcmp(const char *s1, const char *s2)
+/*
+** Does the exact same thing as regular strcmp, but returns the reverse result
+** instead, for sorting reverse alphabetically.
+*/
+
+int	ft_strcmp_rev(const char *s1, const char *s2)
 {
-	return (ft_strncmp(s1, s2, ft_strlen(s1) + 1));
+	return (ft_strncmp(s2, s1, ft_strlen(s1) + 1));
 }
