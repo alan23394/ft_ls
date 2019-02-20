a01280d037b1a8e4023f54ea4e80b8175a43682a
Alan Barnett <alanbarnett328@gmail.com>
2019-02-19 19:49:58 -0800

    Reorganize dir struct members, remove files tree

--- includes/dirs.h
+++ includes/dirs.h
@@ -6,7 +6,7 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 11:53:28 by abarnett          #+#    #+#             */
-/*   Updated: 2019/02/13 13:45:40 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/02/14 15:35:21 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 # define T_DIR(t) ((t_dir *)((t)->content))
 
 typedef struct s_binarytree	t_binarytree;
+typedef struct s_flags		t_flags;
 
 typedef struct		s_dir
 {
 	char			*name;
-	t_binarytree	*files;
+	unsigned int	links_maxlen;
 	unsigned int	user_maxlen;
 	unsigned int	group_maxlen;
-	unsigned int	links_maxlen;
 	unsigned int	bytes_maxlen;
 	unsigned int	total_size;
 }					t_dir;
@@ -31,6 +31,7 @@ typedef struct		s_dir
 t_dir				*new_dir(char *item);
 void				insert_dir(t_binarytree **dirs, char *insert,
 						int (*compare)(char *s1, char *s2));
+void				print_dir(t_dir *dir, t_binarytree *files, t_flags *flags);
 void				delete_dir(t_dir *dir);
 
 #endif
