5eba70cf7d0c019810c2e158b8ab7d8896a72721
Alan Barnett <alanbarnett328@gmail.com>
2019-02-19 19:51:09 -0800

    Move functions in header, add stupid typedef

--- includes/files.h
+++ includes/files.h
@@ -6,7 +6,7 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 11:21:14 by abarnett          #+#    #+#             */
-/*   Updated: 2019/02/13 12:52:16 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/02/14 16:20:54 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,10 @@ t_file				*new_file(char *filename, char *path_to_file, int info);
 void				get_info(t_file *file, char *path);
 void				insert_file(t_binarytree **files, t_file *insert,
 						int (*compare)(char *s1, char *s2));
-void				print_file(t_file *file);
-void				print_file_long(t_file *file);
 void				delete_file(t_file *file);
+
+typedef struct s_dir	t_dir;
+void				print_file(t_file *file);
+void				print_file_long(t_file *file, t_dir *dir);
 
 #endif
