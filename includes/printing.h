82fdae3c7d2df1a80966524f29a114303ea717c9
Alan Barnett <alanbarnett328@gmail.com>
2019-03-03 20:09:05 -0800

    Add prototypes for color printing

--- includes/printing.h
+++ includes/printing.h
@@ -6,7 +6,7 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 22:34:54 by abarnett          #+#    #+#             */
-/*   Updated: 2019/02/21 22:43:36 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/03/03 20:05:30 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ typedef struct		s_flags t_flags;
 
 void				print_dir(t_dir *dir, t_binarytree *files, t_flags *flags);
 void				print_file(t_file *file);
+void				print_file_color(t_file *file);
 void				print_file_long(t_file *file, t_dir *dir);
+void				print_file_long_color(t_file *file, t_dir *dir);
 
 #endif
