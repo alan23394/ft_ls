477149d25968e877acf82ba153d1ea87636a919d
Alan Barnett <alanbarnett328@gmail.com>
2019-03-03 18:34:43 -0800

    Fix char arrays by adding null terminators

--- includes/config.h
+++ includes/config.h
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 16:52:34 by alan              #+#    #+#             */
-/*   Updated: 2019/02/24 18:32:27 by alan             ###   ########.fr       */
+/*   Updated: 2019/03/01 21:13:30 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 # define COLOR_LINK "\e[1;36m"
 # define COLOR_ZIP "\e[0;35m"
 # define COLOR_PICTURE "\e[0;32m"
-# define COLOR_VIDEO "\e[1;33m"
 # define COLOR_AUDIO "\e[0;36m"
+# define COLOR_VIDEO "\e[0;33m"
 # define COLOR_NORM "\e[m"
 
 static const char	*zip_extensions[] = {
@@ -29,6 +29,7 @@ static const char	*zip_extensions[] = {
 	".tar",
 	".gz",
 	".tar.gz",
+	0
 };
 
 static const char	*picture_extensions[] = {
@@ -37,17 +38,20 @@ static const char	*picture_extensions[] = {
 	".jpeg",
 	".bmp",
 	".gif",
+	0
 };
 
 static const char	*video_extensions[] = {
 	".webm",
 	".mp4",
+	0
 };
 
 static const char	*audio_extensions[] = {
 	".mp3",
 	".wav",
 	".flac",
+	0
 };
 
 #endif
