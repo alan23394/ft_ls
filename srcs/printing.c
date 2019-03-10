36737c6c6b3286b00a438f4bbf47c5c8752bb340
Alan Barnett <alanbarnett328@gmail.com>
2019-03-09 20:46:00 -0800

    Add extended attribute character to printing functions

--- srcs/printing.c
+++ srcs/printing.c
@@ -6,7 +6,7 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/01 14:16:52 by abarnett          #+#    #+#             */
-/*   Updated: 2019/03/09 00:33:38 by alan             ###   ########.fr       */
+/*   Updated: 2019/03/09 19:23:28 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,19 +55,18 @@ void	print_file_long(t_file *file, t_dir *dir)
 			if (size_of_link != -1)
 			{
 				link[size_of_link] = 0;
-				ft_printf("%s  %*u %-*s  %-*s  %*lu %s %s -> %s\n",
-						file->rights, dir->links_maxlen, file->links,
-						dir->user_maxlen, file->user, dir->group_maxlen,
-						file->group, dir->bytes_maxlen, file->bytes, file->date,
-						file->name, link);
+				ft_printf("%s%c %*u %-*s  %-*s  %*lu %s %s -> %s\n",
+					file->rights, file->ex_attr, dir->links_maxlen, file->links,
+					dir->user_maxlen, file->user, dir->group_maxlen,
+					file->group, dir->bytes_maxlen, file->bytes, file->date,
+					file->name, link);
 			}
 		}
 		else
-			ft_printf("%s  %*u %-*s  %-*s  %*lu %s %s\n",
-					file->rights, dir->links_maxlen, file->links,
-					dir->user_maxlen, file->user, dir->group_maxlen,
-					file->group, dir->bytes_maxlen, file->bytes, file->date,
-					file->name);
+			ft_printf("%s%c %*u %-*s  %-*s  %*lu %s %s\n",
+				file->rights, file->ex_attr, dir->links_maxlen, file->links,
+				dir->user_maxlen, file->user, dir->group_maxlen, file->group,
+				dir->bytes_maxlen, file->bytes, file->date, file->name);
 	}
 }
 
@@ -84,18 +83,18 @@ void	print_file_long_color(t_file *file, t_dir *dir)
 			if (size_of_link != -1)
 			{
 				link[size_of_link] = 0;
-				ft_printf("%s  %*u %-*s  %-*s  %*lu %s %s%s%s -> %s\n",
-						file->rights, dir->links_maxlen, file->links,
-						dir->user_maxlen, file->user, dir->group_maxlen,
-						file->group, dir->bytes_maxlen, file->bytes, file->date,
-						file->color, file->name, COLOR_NORM, link);
+				ft_printf("%s%c %*u %-*s  %-*s  %*lu %s %s%s%s -> %s\n",
+					file->rights, file->ex_attr, dir->links_maxlen, file->links,
+					dir->user_maxlen, file->user, dir->group_maxlen,
+					file->group, dir->bytes_maxlen, file->bytes, file->date,
+					file->color, file->name, COLOR_NORM, link);
 			}
 		}
 		else
-			ft_printf("%s  %*u %-*s  %-*s  %*lu %s %s%s%s\n",
-					file->rights, dir->links_maxlen, file->links,
-					dir->user_maxlen, file->user, dir->group_maxlen,
-					file->group, dir->bytes_maxlen, file->bytes, file->date,
-					file->color, file->name, COLOR_NORM);
+			ft_printf("%s%c %*u %-*s  %-*s  %*lu %s %s%s%s\n",
+				file->rights, file->ex_attr,dir->links_maxlen, file->links,
+				dir->user_maxlen, file->user, dir->group_maxlen, file->group,
+				dir->bytes_maxlen, file->bytes, file->date, file->color,
+				file->name, COLOR_NORM);
 	}
 }
