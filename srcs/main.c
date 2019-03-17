dc61ab8fd234b613f4bbb5c03f14a9a520089c18
Alan Barnett <alanbarnett328@gmail.com>
2019-03-17 00:51:29 -0700

    chore(print_illegal_option): function finished
    argv will be aligned to the bad character, so all I have to do is send
    it. I also changed the valid characters to print from my macro, so it'll
    always be up to date.

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,21 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 18:25:08 by abarnett          #+#    #+#             */
-/*   Updated: 2019/03/09 14:28:20 by alan             ###   ########.fr       */
+/*   Updated: 2019/03/17 03:40:25 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int				main(int argc, char **argv)
+int		print_illegal_option(char illegal_op)
+{
+	ft_printfd(2,
+		"ft_ls: illegal option -- %c\nusage: ft_ls [-%s] [file ...]\n",
+		illegal_op, ALL_OPTIONS);
+	return (1);
+}
+
+int		main(int argc, char **argv)
 {
 	t_flags			flags;
 	t_binarytree	*dirs;
@@ -20,7 +28,7 @@ int				main(int argc, char **argv)
 	(void)argc;
 	flags.options = get_options(&argv);
 	if (flags.options == -1)
-		return (-1);
+		return (print_illegal_option(**argv));
 	flags.compare = get_cmp_function(flags.options);
 	flags.print = get_print_func(flags.options);
 	dirs = 0;
