654e329ebfe864ce20efffb64f83ca1551eeb6ca
Alan Barnett <alanbarnett328@gmail.com>
2019-04-04 15:39:23 -0700

    norm(colors): properly prefix global variables
    My global variables for the file extensions were not named correctly,
    they needed to be prefixed with a g_

--- srcs/colors.c
+++ srcs/colors.c
@@ -6,7 +6,7 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/01 15:17:01 by abarnett          #+#    #+#             */
-/*   Updated: 2019/04/04 14:43:46 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/04/04 15:36:29 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,19 +104,19 @@ static int		check_extension_list(char *filename,
 
 static char		*check_extension(char *filename)
 {
-	if (check_extension_list(filename, zip_extensions))
+	if (check_extension_list(filename, g_zip_extensions))
 	{
 		return (COLOR_ZIP);
 	}
-	if (check_extension_list(filename, picture_extensions))
+	if (check_extension_list(filename, g_picture_extensions))
 	{
 		return (COLOR_PICTURE);
 	}
-	if (check_extension_list(filename, video_extensions))
+	if (check_extension_list(filename, g_video_extensions))
 	{
 		return (COLOR_VIDEO);
 	}
-	if (check_extension_list(filename, audio_extensions))
+	if (check_extension_list(filename, g_audio_extensions))
 	{
 		return (COLOR_AUDIO);
 	}
