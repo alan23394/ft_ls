998307a88c5a10944a84eacf1b6d289862e3d598
Alan Barnett <alanbarnett328@gmail.com>
2019-04-04 15:52:23 -0700

    norm(ft_ls.h): fix forward declarations

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,17 +6,17 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 18:23:26 by abarnett          #+#    #+#             */
-/*   Updated: 2019/04/04 15:42:35 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/04/04 15:52:02 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
-typedef struct	s_file t_file;
-typedef struct	s_dir t_dir;
-typedef struct	s_flags t_flags;
-typedef struct	s_binarytree t_binarytree;
+typedef struct s_file		t_file;
+typedef struct s_dir		t_dir;
+typedef struct s_flags		t_flags;
+typedef struct s_binarytree	t_binarytree;
 
 char			*get_dirname(char *cur, char *add);
 void			update_dir(t_dir *dir, t_file *file);
