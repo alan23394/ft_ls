a6013dde92eccfea7c105e4b693560f83ee689cc
Alan Barnett <alanbarnett328@gmail.com>
2019-04-04 18:31:42 -0700

    feat(-1): add new flag for single column
    This adds the new flag to the options string, and also into the enum.
    I also update some of the enum comment, since I've taken out the m
    maximum size variable. I also gave my enum a type, so I can use it
    as a type while dealing with the flags. This is nice because now, i
    don't think I have to worry about the size or anything.

--- includes/flags.h
+++ includes/flags.h
@@ -6,14 +6,14 @@
 /*   By: abarnett <alanbarnett328@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 21:20:13 by abarnett          #+#    #+#             */
-/*   Updated: 2019/04/04 15:41:24 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/04/04 18:23:05 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FLAGS_H
 # define FLAGS_H
 
-# define ALL_OPTIONS "aGlrRt"
+# define ALL_OPTIONS "aGlrRt1"
 
 /*
 ** These are the bit values for the ls flags (OP for option). When you insert
@@ -22,30 +22,28 @@
 **
 ** The values are used as indexes for a function pointer array. It is set up
 ** like this to allow the easy combination of flags, for things like -tr, when
-** you need a specific function to sort by reverse time. The last value is used
-** as the size of the function pointer array, so make sure it is the bitwise
-** addition of all of the previous flags (one power of 2 higher should always
-** work).
+** you need a specific function to sort by reverse time.
 */
 
-enum {
+enum OPS {
 	OP_ALL = 0x1,
 	OP_COLOR = 0x2,
 	OP_LONG = 0x4,
 	OP_REV = 0x8,
 	OP_RECUR = 0x10,
 	OP_TIME = 0x20,
-	FLAG_CLI = 0x40,
+	OP_ONE = 0x40,
+	FLAG_CLI = 0x80,
 };
 
 typedef struct	s_flags
 {
-	int			options;
+	enum OPS	options;
 	int			(*compare)();
 	void		(*print)();
 }				t_flags;
 
-int				get_options(char ***argv);
+enum OPS		get_options(char ***argv);
 void			*get_cmp_function(int flags);
 void			*get_print_func(int flags);
 
